44586d7d19ba9842eb338fb917193417bf5d5169
maxbittker <maxbittker@gmail.com>
2018-02-17 13:34:17 -0800

    scoll to view

--- overlay.js
+++ overlay.js
@@ -46,18 +46,36 @@ function setupOverlay() {
       //not sure what do do with this predicate right now
       return;
     }
+    
+    let existingOverlay= document.getElementById("regl-overlay-error");
+    if(existingOverlay){
+      existingOverlay.parentNode.removeChild(existingOverlay);
+    }
+
     let overlay = document.createElement("div");
+    overlay.id = "regl-overlay-error";
+    
     let textChunks = data.extra.arguments[0].split("%c");
     let chunkStyles = data.extra.arguments.slice(1);
+
     textChunks.map((s, i) => {
       let node = document.createElement("span");
       node.textContent = s;
-      node.style = chunkStyles[i - 1];
+      let style = chunkStyles[i - 1];
+      node.style = style;
+      if (style && style.includes("yellow")) {
+        node.id = "regl-overlay-error-loc";
+      }
       overlay.appendChild(node);
     });
     overlay.style =
       " display: inline-block; unicode-bidi: embed; font-family: monospace; white-space: pre-wrap; width:100%;";
     document.body.appendChild(overlay);
+    window.setTimeout(() => {
+      document
+        .getElementById("regl-overlay-error-loc")
+        .scrollIntoView({ block: "center" });
+    }, 200);
   });
 }
 module.exports = { setupOverlay };
